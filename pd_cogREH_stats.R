d3b8c1dbd55aeaf3d270e6cbba6e3916fb4047fd
josefmana <josef.mana@protonmail.com>
2023-05-20 09:08:06 +0200

    stats init ...

--- pd_cogREH_stats.R
+++ pd_cogREH_stats.R
@@ -1,11 +1,11 @@
 # In this script we first prepare tables for a suitable format then pre-porocess the data, fit series of models and summarise
 # the results. The modelling strategy is that of fitting LMMs with increasingly more complex structure of predictors with
-# subsequent model comparison for evalauting the importance of added predictors above and beyond predictors already included.
+# subsequent model comparison for evaluating the importance of added predictors above and beyond predictors already included.
 
 # list packages to be used
 pkgs <- c( "rstudioapi", # setting working directory via RStudio API
            "tidyverse", "dplyr", # data wrangling
-           "lme4", # model fitting
+           "lme4", "lmerTest", # frequentist stats
            "performance", # model quality checking
            "openxlsx" # reading and writing .xlsx
            )
@@ -33,6 +33,9 @@ d <- list(
   fol = read.xlsx( "_no_github/PN celkem_exp_con_final.xlsx", sheet = "Retest po 1 roce" )
 )
 
+# read a list of included patients
+inc <- read.xlsx( "_no_github/PN celkem_exp_con_final.xlsx", sheet = "VSTUP_RETEST PO 3 MESICICH" )$A_Code
+
 # get rid of the prefixes and add a variable denoting the assessment type
 for ( i in names(d) ) {
   names(d[[i]]) <- names(d[[i]]) %>% sub("^[^_]*_", "", . )
@@ -59,6 +62,12 @@ d0 <- do.call( rbind.data.frame, d ) %>%
           Stroop_color_words = ifelse( Stroop_color_words > 240, 240, Stroop_color_words )
           )
 
+# rename DRS-2 scores such that they don't include slashes
+names(d0)[ grepl("MDRS",names(d0)) ] <- ( names(d0)[ grepl("MDRS",names(d0)) ] %>% strsplit( "_(?!.*_)", perl = T ) %>% do.call( cbind, . ) )[1, ]
+
+# keep only included patients
+d1 <- d0[ d0$Code %in% inc , ]
+
 
 # ---- data set description ----
 
@@ -68,10 +77,10 @@ desc <- lapply( data.frame( n = "n", mean = "mean", sd = "sd", median = "median"
                 function(i)
                   matrix(
                     # set-up an empty matrix for each measure/stat combination
-                    data = NA, nrow = length( names(d0)[-c(1,2,ncol(d0))] ), ncol = 6,
+                    data = NA, nrow = length( names(d0)[-c(1,2,ncol(d1))] ), ncol = 6,
                     # give names to rows and columns
                     dimnames = list(
-                      x = names(d0)[-c(1,2,ncol(d0))],
+                      x = names(d1)[-c(1,2,ncol(d0))],
                       y = expand.grid( unique(d0$assessment), unique(d0$GROUP_EXP_CON) ) %>%
                         mutate( V3 = paste( Var2, Var1, sep = "_" ) ) %>%
                         select(V3) %>% unlist()
@@ -83,7 +92,7 @@ desc <- lapply( data.frame( n = "n", mean = "mean", sd = "sd", median = "median"
 for ( i in names(desc$n) ) {
   
   grp <- strsplit(i,"_")[[1]] # prepare a grouping variable based on the column name
-  desc$n[ "Gender", i ] <- with( d0, table( Gender[ GROUP_EXP_CON == grp[1] & assessment == grp[2] ] ) ) %>% paste(., collapse = "/" ) # fill-in gender distributions
+  desc$n[ "Gender", i ] <- with( d1, table( Gender[ GROUP_EXP_CON == grp[1] & assessment == grp[2] ] ) ) %>% paste(., collapse = "/" ) # fill-in gender distributions
 
 } 
 
@@ -94,16 +103,64 @@ for ( i in names(desc$n) ) {
   vrs <- rownames(desc$n)[-1] # variables to be described
   
   # count non-zero observations for each variable/group pair
-  desc$n[ vrs , i ] <- apply( d0[ with( d0, GROUP_EXP_CON == grp[1] & assessment == grp[2] ), vrs ], 2, function(x) sum( !is.na(x) ) )
+  desc$n[ vrs , i ] <- apply( d1[ with( d1, GROUP_EXP_CON == grp[1] & assessment == grp[2] ), vrs ], 2, function(x) sum( !is.na(x) ) )
   
   # calculate summary statistics
   # variables stay the same as for the counts above
-  for ( j in names(desc)[-1] ) desc[[j]][ vrs, i ] <- apply( d0[ with( d0, GROUP_EXP_CON == grp[1] & assessment == grp[2] ), vrs ], 2, j, na.rm = T )
+  for ( j in names(desc)[-1] ) desc[[j]][ vrs, i ] <- apply( d1[ with( d1, GROUP_EXP_CON == grp[1] & assessment == grp[2] ), vrs ], 2, j, na.rm = T )
   
 }
 
 # save the descriptives as .xlsx
 write.xlsx( desc, "tabs/pd_cogREH_desc.xlsx", rowNames = T )
+
+
+# ---- data pre-processing ----
+
+# check that there's exactly one observation (or lack of thereof) in each patient/assessment pair
+with( d1, table( Code, assessment) ) %>% as.data.frame() %>% filter( Freq != 1 ) # print cells with counts different from one
+
+# re-format the assessment variable to an ordered factor
+d1$assessment <- factor( d1$assessment, levels = c("baseline","retest","follow-up"), ordered = T )
+
+# read a list of likelihoods for outcome variables
+liks <- read.csv( "tabs/likelihoods.csv", sep = "," )
+
+# transform relevant variables:
+# (i) log transform reaction times
+# (ii) for binomial variables prepare a column with number of failures
+# (iii) in-sample standardise Gaussian outcomes
+for ( i in c( with( liks, out[trans == "log"] ) %>% na.omit() ) ) d1[ , i] <- log( d1[ , i] )
+for ( i in c( with( liks, out[lik == "binomial"] ) %>% na.omit() ) ) d1[ , i] <- d1[ , i] / with( liks, ceil[out==i] )
+for ( i in c( with( liks, out[lik == "gaussian"] %>% na.omit() ) ) ) d1[ , i] <- ( d1[ , i] - mean( d1[ , i], na.rm = T ) ) / sd( d1[ , i], na.rm = T )
+
+
+# ---- representation: statistical model ----
+
+# set-up predictor structures
+prds <- data.frame( nak = " ~ 1 + (1 | Code)", # 1) the "naked" model
+                    ass = " ~ 1 + assessment + (1 | Code)", # 2) the "test-retest" model
+                    grp = " ~ 1 + assessment + GROUP_EXP_CON + (1 | Code)", # 3) the "different people" model
+                    int = " ~ 1 + assessment * GROUP_EXP_CON + (1 | Code)" # 4) the "rehabilitation works" model
+                    )
+
+# prepare a set of formulas for fitting
+f <- lapply( setNames( liks$out, liks$out ), function(i) lapply( prds, function(j) paste0( i, j ) %>% as.formula ) )
+
+
+# ---- implementation: learning from the data  ----
+
+# let's fit it
+m <- lapply( setNames( names(f), names(f) ),
+             function(i)
+               lapply( setNames( names(prds), names(prds) ),
+                       function(j) {
+                         if ( with( liks, lik[ out == i ] ) == "gaussian" ) return( lmer( f[[i]][[j]], data = d1, REML = F ) )
+                         if ( with( liks, lik[ out == i ] ) == "poisson" ) return( glmer( f[[i]][[j]], data = d1, family = poisson() ) )
+                         if ( with( liks, lik[ out == i ] ) == "binomial" ) return( glmer( f[[i]][[j]], data = d1, family = binomial(), weights = rep( with( liks, ceil[ out == i ] ) , nrow(d1) ) ) )
+                       }
+                 )
+             )
 
 
 # ---- session info ----
